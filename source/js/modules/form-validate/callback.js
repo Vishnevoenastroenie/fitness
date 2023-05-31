5c73c8642ff01ce5c310391e5d1400e38365b089
Наталья Лягина <Vischnevoenastroenie@yandex.ru>
2023-05-31 19:00:07 +0400

    Откат к предыдущему состояянию формы

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,51 +1,7 @@
 
 const baseSuccessCallback = (event) => {
   event.preventDefault();
-  const form = document.querySelector('form');
-  const data = new FormData(form);
-  const url = 'https://echo.htmlacademy.ru';
-
-  fetch(url, {
-    method: 'POST',
-    body: data,
-  }).then((response) => {
-    if (response.ok) {
-      const entries = Array.from(data.entries());
-      const modal = document.createElement('div');
-      const successMessage = document.createElement('p');
-      successMessage.textContent = 'Форма успешно отправлена';
-      modal.appendChild(successMessage);
-      const dataMessage = document.createElement('p');
-      dataMessage.textContent = 'Данные которые были отправлены:';
-      modal.appendChild(dataMessage);
-      entries.forEach((entry) => {
-        const p = document.createElement('p');
-        p.textContent = `${entry[0]}: ${entry[1]}`;
-        modal.appendChild(p);
-      });
-      const button = document.createElement('button');
-      button.style.backgroundColor = 'transparent';
-      button.style.cursor = 'pointer';
-      button.textContent = 'Закрыть';
-      button.addEventListener('click', () => {
-        modal.remove();
-      });
-      modal.appendChild(button);
-      modal.style.position = 'fixed';
-      modal.style.top = '50%';
-      modal.style.left = '50%';
-      modal.style.transform = 'translate(-50%, -50%)';
-      modal.style.backgroundColor = 'white';
-      modal.style.padding = '40px';
-      modal.style.borderRadius = '10px';
-      modal.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
-      modal.style.zIndex = '100000';
-      document.body.appendChild(modal);
-    } else {
-      // eslint-disable-next-line no-alert
-      alert('Ошибка при отправке формы');
-    }
-  });
+  // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
 };
 
 const baseErrorCallback = (event) => {
